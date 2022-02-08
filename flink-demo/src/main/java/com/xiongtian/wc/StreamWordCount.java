b273ba7cb9f0d57c1200991c853f9f39eeb64051
熊天 <xiongtian_email@163.com>
2022-02-09 00:26:31 +0800

    flink-demo 从socket文本流读取数据

--- flink-demo/src/main/java/com/xiongtian/wc/StreamWordCount.java
+++ flink-demo/src/main/java/com/xiongtian/wc/StreamWordCount.java
@@ -2,6 +2,7 @@ package com.xiongtian.wc;
 
 import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -20,8 +21,17 @@ public class StreamWordCount {
         env.setParallelism(3);
 
         // 从文件中读取数据
-        String inputPath = "D:\\mycode\\java_code\\big-data-demo\\flink-demo\\src\\main\\resources\\hello.txt";
-        DataStream<String> inputDataStream = env.readTextFile(inputPath);
+//        String inputPath = "D:\\mycode\\java_code\\big-data-demo\\flink-demo\\src\\main\\resources\\hello.txt";
+//        DataStream<String> inputDataStream = env.readTextFile(inputPath);
+
+        // 用parameter tool工具从程序启动参数中提取配置项
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+
+        String host = parameterTool.get("host");
+        int port = parameterTool.getInt("port");
+
+        // 从socket文本流读取数据
+        DataStream<String> inputDataStream = env.socketTextStream(host, port);
 
         // 基于数据流进行转换计算
         SingleOutputStreamOperator<Tuple2<String, Integer>> resultStram = inputDataStream.flatMap(new WordCount.MyFlatMapper())
