de4b5d807e1212fc5e1e5f61dc46cf4891d5d577
熊天 <xiongtian_email@163.com>
2022-02-12 15:11:47 +0800

    flink-demo transform connect合流

--- flink-demo/src/main/java/com/xiongtian/apitest/transform/TransformTest4_MultipleStream.java
+++ flink-demo/src/main/java/com/xiongtian/apitest/transform/TransformTest4_MultipleStream.java
@@ -1,10 +1,16 @@
 package com.xiongtian.apitest.transform;
 
 import com.xiongtian.apitest.beans.SensorReading;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.collector.selector.OutputSelector;
+import org.apache.flink.streaming.api.datastream.ConnectedStreams;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoMapFunction;
+import scala.Tuple2;
+import scala.Tuple3;
 
 import java.util.Collections;
 
@@ -42,6 +48,30 @@ public class TransformTest4_MultipleStream {
         lowTempStream.print("low");
         allTempStream.print("all");
 
+        // 2. 合流connect,将高温流转换成二元组类型，与低温流连接和并之后，输出状态信息
+        SingleOutputStreamOperator<Tuple2<String, Double>> warningStream = highTempStream.map(new MapFunction<SensorReading, Tuple2<String, Double>>() {
+            @Override
+            public Tuple2<String, Double> map(SensorReading sensorReading) throws Exception {
+                return new Tuple2<>(sensorReading.getId(), sensorReading.getTemperature());
+            }
+        });
+
+        ConnectedStreams<Tuple2<String, Double>, SensorReading> connectedStreams = warningStream.connect(lowTempStream);
+        SingleOutputStreamOperator<Object> resultStream = connectedStreams.map(new CoMapFunction<Tuple2<String, Double>, SensorReading, Object>() {
+            @Override
+            public Object map1(Tuple2<String, Double> stringDoubleTuple2) throws Exception {
+                return new Tuple3<>(stringDoubleTuple2._1, stringDoubleTuple2._2, "high temp warning");
+            }
+
+            @Override
+            public Object map2(SensorReading sensorReading) throws Exception {
+                return new Tuple2<>(sensorReading.getId(), "normal");
+            }
+
+        });
+
+
+        resultStream.print();
 
         env.execute();
 
